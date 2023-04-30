ac556a61d1ec8bc273a097856fbaff4688b7b711
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-04-30 06:18:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+app.get('/categories', (req, res) => {
+  res.send(categories)
+})
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find(n => n._id === id )
+  res.send(selectedNews)
+})
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) === 0) {
+    res.send(news)
+  } else {
+    const newsCategory = news.filter(n => n.category_id === id)
+    res.send(newsCategory)
+  }
+})
+
+
+
+app.get('/', (req, res) => {
+  res.send('welcome to the news Api. Search by category and news with dynamically')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
